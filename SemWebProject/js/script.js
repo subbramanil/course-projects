643cfd2ab9cf32ca2b51cec7b9a7eda8932dc4c2
Subbramanian Lakshmanan <sxl146830@utdallas.edu>
2015-04-18 19:28:07 -0500

    Integrated with all the screens
    Updated page templates with Bootstrap CSS
    Added Bar and Pie-charts for displaying surgery & patients types.

--- SemWebProject/js/script.js
+++ SemWebProject/js/script.js
@@ -65,12 +65,19 @@ function stateLookup(code) {
     return usStates[code].abbreviation;
 }
 
-
 // load google visualization packages
-google.load('visualization', '0', {'packages': ['geomap', 'corechart', 'geochart']});
+google.load('visualization', '0', {'packages': ['geomap', 'corechart', 'geochart', 'bar']});
 
-// set callback function for drawing visualizations
-//google.setOnLoadCallback(drawSurgeryMap);
+/** Script for Styles */
+$('#tab1 a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
+$('#tab2 a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
+
 
 function setupNewData(arrayData, columnName, colNum) {
     console.log("setupNewData() entry");
@@ -107,6 +114,24 @@ var query_surgeryTypes = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-n
     "group by ?state " +
     "order by ?state";
 
+var query_surgeryTypesByState = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    "PREFIX : <http://data-gov.tw.rpi.edu/vocab/p/1202/> " +
+    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+    "SELECT ?state ?city (sum(xsd:int(?gen_count)) as ?count_gen_surgery) (sum(xsd:int(?card_count)) as ?count_card_surgery) (sum(xsd:int(?ortho_count)) as ?count_ortho_surgery) (sum(xsd:int(?vas_count)) as ?count_vascular_surgery) (sum(xsd:int(?other_count)) as ?count_other_surgery)" +
+    "WHERE { " +
+    "?sub :state ?state. " +
+    "?sub :city ?city. " +
+    "?sub :facility_name ?fac. " +
+    "?sub :general_surgery ?gen_count. " +
+    "?sub :cardiac ?card_count. " +
+    "?sub :orthopedic ?ortho_count. " +
+    "?sub :vascular_surgery ?vas_count. " +
+    "?sub :other_surgery ?other_count." +
+    "}" +
+    "group by ?state ?city " +
+    "order by ?state";
+
 var query_emergBeds = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
     "PREFIX : <http://data-gov.tw.rpi.edu/vocab/p/1202/> " +
@@ -138,4 +163,39 @@ var query_loadInfraData = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-
     "?sub2 :carf_accreditation ?carf_accreditation. " +
     "?sub2 :joint_commission_accreditation ?joint_commission_accreditation" +
     "}" +
+    "order by ?state";
+
+var query_loadTotalPatientsData = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    "PREFIX : <http://data-gov.tw.rpi.edu/vocab/p/1202/> " +
+    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+    "SELECT ?state (sum(xsd:int(?afr_amr_patient_out_count)) + sum(xsd:int(?white_outpatient_count)) + sum(xsd:int(?other_outpatient_count)) as ?total_out_patient) " +
+    "(sum(xsd:int(?afr_amr_patient_in_count)) + sum(xsd:int(?white_inpatient_count)) + sum(xsd:int(?other_inpatient_count)) as ?total_in_patient)" +
+    "WHERE { " +
+    "?sub1 :state ?state. " +
+    "?sub :african_american_outpatient ?afr_amr_patient_out_count. " +
+    "?sub :white_outpatient ?white_outpatient_count. " +
+    "?sub :other_outpatient ?other_outpatient_count. " +
+    "?sub :african_american_inpatient ?afr_amr_patient_in_count. " +
+    "?sub :white_inpatient ?white_inpatient_count. " +
+    "?sub :other_inpatient ?other_inpatient_count. " +
+    "}" +
+    "group by ?state " +
+    "order by ?state";
+
+var query_loadPatientsByRace = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    "PREFIX : <http://data-gov.tw.rpi.edu/vocab/p/1202/> " +
+    "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+    "SELECT ?state (sum(xsd:int(?afr_amr_inpatient_count)) as ?count_afr_amr_inpatient_count) (sum(xsd:int(?afr_amr_outpatient_count)) as ?count_afr_amr_outpatient_count) (sum(xsd:int(?white_inpatient)) as ?count_white_inpatient) (sum(xsd:int(?white_outpatient)) as ?count_white_outpatient) (sum(xsd:int(?other_inpatient)) as ?count_other_inpatient) (sum(xsd:int(?other_outpatient)) as ?count_other_outpatient)  " +
+    "WHERE { " +
+    "?sub1 :state ?state. " +
+    "?sub1 :african_american_inpatient ?afr_amr_inpatient_count. " +
+    "?sub1 :african_american_outpatient ?afr_amr_outpatient_count. " +
+    "?sub1 :white_inpatient ?white_inpatient. " +
+    "?sub1 :white_outpatient ?white_outpatient." +
+    "?sub1 :other_inpatient ?other_inpatient. " +
+    "?sub1 :other_outpatient ?other_outpatient " +
+    "} " +
+    "group by ?state " +
     "order by ?state";
--- SemWebProject/js/vizScript.js
+++ SemWebProject/js/vizScript.js
@@ -21,8 +21,12 @@ adminApp.config(['$routeProvider',
                 templateUrl: 'pages/accreditionStatus.html',
                 controller: 'AccredController'
             }).
+            when('/Patients', {
+                templateUrl: 'pages/patient.html',
+                controller: 'PatientController'
+            }).
             otherwise({
-                redirectTo: '/index'
+                redirectTo: '/Dashboard'
             });
     }]);
 
@@ -31,7 +35,9 @@ google.load('visualization', '0', {'packages': ['geomap', 'corechart', 'geochart
 
 adminApp.controller('DashBoardController', function ($scope) {
 
-    $scope.message = 'This is Dashboard screen';
+    $scope.tabClick = function ($event) {
+        $event.preventDefault();
+    };
 
 });
 
@@ -44,16 +50,49 @@ adminApp.controller('SurgeryController', ['$scope', '$http', function ($scope, $
         {name: 'Vascular Surgery', col: 4},
         {name: 'Other Surgery', col: 5}
     ];
+    $scope.userChoice = $scope.choices[0];
+
+    var surgeryData = null;
+    function prepareSurgeryData(){
+        console.log("SurgeryController.prepareSurgeryData() entry");
+        var url = serverURL + encodeURIComponent(query_surgeryTypesByState) + "&output=csv";
+        console.log(url);
+        $http.get(url).
+            success(function (csvData, status, headers, config) {
+                surgeryData = $.csv.toArrays(csvData, {onParseValue: $.csv.hooks.castToScalar});
+                for(var i=0;i<surgeryData.length;i++){
+                    for(var j=0;j<surgeryData[i].length;j++){
+                        if(surgeryData[i][j]==null){
+                            surgeryData[i][j] = 0;
+                        }
+                    }
+                }
+            }).
+            error(function (data, status, headers, config) {
+                console.log("error in ajax call:SurgeryController.prepareStateData()");
+            });
+        console.log("SurgeryController.prepareSurgeryData() exit");
+    }
 
     $scope.change = function () {
         console.log("SurgeryController.change() entry");
         console.log($scope.userChoice);
+        prepareSurgeryData();
         var url = serverURL + encodeURIComponent(query_surgeryTypes) + "&output=csv";
         console.log(url);
         $http.get(url).
             success(function (csvData, status, headers, config) {
                 var data = $.csv.toArrays(csvData, {onParseValue: $.csv.hooks.castToScalar});
-                var newData = setupNewData(data, "General Surgery Count", 1);
+                console.log(data);
+                for(var i=0;i<data.length;i++){
+                    for(var j=0;j<data[i].length;j++){
+                        if(data[i][j]==null){
+                            data[i][j] = 0;
+                        }
+                    }
+                }
+                console.log($scope.userChoice);
+                var newData = setupNewData(data, $scope.userChoice.name, $scope.userChoice.col);
 
                 var options = {};
                 options['region'] = 'US';   // show US map
@@ -62,9 +101,60 @@ adminApp.controller('SurgeryController', ['$scope', '$http', function ($scope, $
                 options['height'] = 600;
                 options['colors'] = [0xADEBAD, 0x5C85FF, 0xFF1919];
 
-                var divId = document.getElementById('chart');
-                var chart = new google.visualization.GeoMap(divId);
-                chart.draw(newData, options );
+                var geoChart = new google.visualization.GeoMap(document.getElementById('geoChart'));
+                google.visualization.events.addListener(geoChart , 'regionClick', function (eventData) {
+                    var region = eventData.region.substr(3);
+                    console.log(region);
+                    $scope.regionName = region;
+
+                    var cityData = [];
+                    var rows = surgeryData.length;
+                    console.log(rows);
+                    for (var i = 1; i < rows; i++ )
+                    {
+                        if(surgeryData[i][0] == region){
+                            cityData.push(surgeryData[i]);
+                        }
+                    }
+                    console.log("No of Cities with Surgery Count:"+cityData.length);
+                    console.log(cityData);
+
+                    var chartData = [];
+                    var barChartHeader = ['City', '# of Surgery'];
+                    chartData.push(barChartHeader);
+                    for(var i=0;i<cityData.length;i++){
+                        var val = cityData[i][$scope.userChoice.col+1];
+                        if(val>0){
+                            chartData.push([cityData[i][1], val]);
+                        }
+                    }
+                    console.log(chartData);
+                    if(chartData.length>1){
+                        $scope.noData = false;
+                        var data = google.visualization.arrayToDataTable(chartData);
+                        console.log(data);
+
+                        var options = {
+                            title: $scope.userChoice.name,
+                            chartArea: {width: '100%'},
+
+                            hAxis: {
+                                title: '# of Surgery',
+                                minValue: 0
+                            },
+                            vAxis: {
+                                title: 'City'
+                            }
+                        };
+                        var barChart = new google.visualization.BarChart(document.getElementById('barChart'));
+                        barChart.draw(data, options);
+                    }else{
+                        $scope.noData = true;
+                    }
+
+                });
+
+                geoChart.draw(newData, options);
             }).
             error(function (data, status, headers, config) {
                 console.log("error in ajax call:SurgeryController.change()");
@@ -178,4 +268,144 @@ adminApp.controller('AccredController', ['$scope', '$http', function ($scope, $h
 
     $scope.message = 'This is Accredition screen';
 
+}]);
+
+adminApp.controller('PatientController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.tabClick = function ($event) {
+        $event.preventDefault();
+    };
+
+    var stateData = null;
+    function preparePatientsByRaceData(){
+        console.log("PatientController.preparePatientsData() entry");
+        var url = serverURL + encodeURIComponent(query_loadPatientsByRace) + "&output=csv";
+        console.log(url);
+        $http.get(url).
+            success(function (csvData, status, headers, config) {
+                stateData = $.csv.toArrays(csvData, {onParseValue: $.csv.hooks.castToScalar});
+            }).
+            error(function (data, status, headers, config) {
+                console.log("error in ajax call:PatientController.preparePatientsData()");
+            });
+        console.log("PatientController.preparePatientsData() exit");
+    }
+
+   /* function preparePatientsByTypeData(){
+        console.log("PatientController.preparePatientsByTypeData() entry");
+        var url = serverURL + encodeURIComponent(query_loadInfraData) + "&output=csv";
+        console.log(url);
+        $http.get(url).
+            success(function (csvData, status, headers, config) {
+                stateData = $.csv.toArrays(csvData, {onParseValue: $.csv.hooks.castToScalar});
+            }).
+            error(function (data, status, headers, config) {
+                console.log("error in ajax call:PatientController.preparePatientsByTypeData()");
+            });
+        console.log("PatientController.preparePatientsByTypeData() exit");
+    }*/
+
+    $scope.drawGeoMap = function () {
+        console.log("PatientController.drawGeoMap() entry");
+        //preparePatientsByRaceData();
+        var patientsData;
+        var url = serverURL + encodeURIComponent(query_loadPatientsByRace) + "&output=csv";
+        console.log(url);
+        $http.get(url).
+            success(function (csvData, status, headers, config) {
+                var patientsData = $.csv.toArrays(csvData, {onParseValue: $.csv.hooks.castToScalar});
+                console.log(patientsData);
+                var newData = [];
+                var rows = patientsData.length;
+                newData.push(["State", "In Patients Count", "Out Patients Count"]);
+                for (var i = 1; i < rows; i++ )
+                {
+                    newData.push([patientsData[i][0], (patientsData[i][1]+patientsData[i][3]+patientsData[i][5]), (patientsData[i][2]+patientsData[i][4]+patientsData[i][6])])
+                }
+                console.log("Total records: "+newData.length);
+                console.log(newData);
+                var inPatientsData = setupNewData(newData , "Total In Patients", 1);
+                var outPatientsData = setupNewData(newData , "Total Out Patients", 2);
+
+                var options = {};
+                options['region'] = 'US';   // show US map
+                options['dataMode'] = 'regions';
+                options['width'] = '100%';
+                options['height'] = 600;
+                options['colors'] = [0xADEBAD, 0x5C85FF, 0xFF1919];
+                var geoInChart = new google.visualization.GeoMap(document.getElementById('geoInChart'));
+                geoInChart .draw(inPatientsData, options );
+
+                google.visualization.events.addListener(geoInChart , 'regionClick', function (eventData) {
+                    var region = eventData.region.substr(3);
+                    console.log(region);
+                    $scope.regionName = region;
+                    var pieData = [];
+                    console.log(patientsData);
+                    var rows = patientsData.length;
+                    for (var i = 1; i < rows; i++) {
+                        if (patientsData[i][0] == region) {
+                            pieData.push(patientsData[i]);
+                        }
+                    }
+                    console.log(pieData);
+
+                    var data = google.visualization.arrayToDataTable([
+                        ['Race', 'Count'],
+                        ['African American', pieData[0][1]],
+                        ['White', pieData[0][3]],
+                        ['Other', pieData[0][5]]
+                    ]);
+
+                    var options = {
+                        title: 'Inpatients'
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('pieInChart'));
+
+                    chart.draw(data, options);
+                });
+
+
+                var geoOutChart = new google.visualization.GeoMap(document.getElementById('geoOutChart'));
+                geoOutChart .draw(outPatientsData, options );
+
+                google.visualization.events.addListener(geoOutChart , 'regionClick', function (eventData) {
+                    var region = eventData.region.substr(3);
+                    console.log(region);
+                    $scope.regionName = region;
+                    var pieData = [];
+                    console.log(patientsData);
+                    var rows = patientsData.length;
+                    for (var i = 1; i < rows; i++) {
+                        if (patientsData[i][0] == region) {
+                            pieData.push(patientsData[i]);
+                        }
+                    }
+                    console.log(pieData);
+
+                    var data = google.visualization.arrayToDataTable([
+                        ['Race', 'Count'],
+                        ['African American', pieData[0][2]],
+                        ['White', pieData[0][4]],
+                        ['Other', pieData[0][6]]
+                    ]);
+
+                    var options = {
+                        title: 'Outpatients'
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('pieOutChart'));
+
+                    chart.draw(data, options);
+                });
+
+            }).
+            error(function (data, status, headers, config) {
+                console.log("error in ajax call:PatientController.drawGeoMap()");
+            });
+
+        console.log("PatientController.drawGeoMap() exit");
+    };
+    google.setOnLoadCallback($scope.drawGeoMap());
 }]);
