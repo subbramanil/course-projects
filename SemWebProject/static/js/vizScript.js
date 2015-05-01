fc0fc0acc8f11061bb2eaffa843a4453077f5f74
Subbramanian Lakshmanan <sxl146830@utdallas.edu>
2015-05-01 16:45:15 -0500

    Minor code re-shuffling

--- SemWebProject/static/js/vizScript.js
+++ SemWebProject/static/js/vizScript.js
@@ -1,6 +1,6 @@
 var adminApp = angular.module('adminApp', ['ngRoute']);
-//var serverURL = "http://localhost:3030/vethealth/query?query=";
-var serverURL = "http://52.10.61.232:3030/ds/query?query=";
+var serverURL = "http://localhost:3030/vethealth/query?query=";
+//var serverURL = "http://52.10.61.232:3030/ds/query?query=";
 
 adminApp.config(['$routeProvider',
     function ($routeProvider) {
